8bb97296157c4df9aa75866e5445a927dd328f23
Pawan Singh Rajwar <pawansinghrajwar@gmail.com>
2024-11-24 13:38:25 +0530

    Coded the set up for main window and canvas dimensions

---
+++ Catch the Ball Game.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import random
+
+# Set up the main window
+root = tk.Tk()
+root.title("Catch the Ball Game")
+root.resizable(False, False)
+
+# Canvas dimensions
+canvas_width = 400
+canvas_height = 600
