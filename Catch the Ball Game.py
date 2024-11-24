366083da2112fa0d9b2c7c5b081aef97b1da8ff3
Pawan Singh Rajwar <pawansinghrajwar@gmail.com>
2024-11-24 13:39:17 +0530

    Updated the code by adding the paddle properties

--- Catch the Ball Game.py
+++ Catch the Ball Game.py
@@ -9,3 +9,14 @@ root.resizable(False, False)
 # Canvas dimensions
 canvas_width = 400
 canvas_height = 600
+
+# Create the canvas
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="lightblue")
+canvas.pack()
+
+# Paddle properties
+paddle_width = 100
+paddle_height = 20
+paddle_color = "darkblue"
+paddle_x = (canvas_width - paddle_width) // 2
+paddle_y = canvas_height - 30
